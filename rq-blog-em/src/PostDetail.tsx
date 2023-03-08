57b5d61eb58dd9def4e09ccc36eafeb6e967eb83
Marknjo <marknjo7@gmail.com>
2023-03-08 17:30:21 +0300

    feat(em-blog): simulate post update  with useMutation hook

--- rq-blog-em/src/PostDetail.tsx
+++ rq-blog-em/src/PostDetail.tsx
@@ -35,15 +35,23 @@ export interface Comment {
   id: string;
 }
 
-async function updatePost(postId: string) {
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/postId/${postId}`,
-    {
-      method: "PATCH",
-      body: JSON.stringify({ data: { title: "REACT QUERY FOREVER!!!!" } }),
-    }
-  );
-  return response.json();
+async function updatePost(postId: number) {
+  // const response = await fetch(
+  //   `https://jsonplaceholder.typicode.com/postId/${postId}`,
+  //   {
+  //     method: "PATCH",
+  //     body: JSON.stringify({ data: { title: "REACT QUERY FOREVER!!!!" } }),
+  //   }
+  // );
+  // return response.json();
+
+  let res = await new Promise((resolve) => {
+    setTimeout(() => resolve(`Success: Post Id (${postId}) updated`), 2000);
+  });
+
+  return {
+    message: res,
+  };
 }
 
 export function PostDetail({ post }: { post: Post }) {
@@ -57,7 +65,8 @@ export function PostDetail({ post }: { post: Post }) {
     queryFn: () => fetchComments(post.id),
   });
 
-  const mutation = useMutation((postId: number) => deletePost(postId));
+  const deleteMutation = useMutation((postId: number) => deletePost(postId));
+  const updateMutation = useMutation((postId: number) => updatePost(postId));
 
   if (isLoading) {
     return (
@@ -84,25 +93,38 @@ export function PostDetail({ post }: { post: Post }) {
   return (
     <>
       <h3 style={{ color: "blue" }}>{post.title}</h3>
-
-      <button onClick={() => mutation.mutate(post.id)}>Delete</button>
-      <button>Update title</button>
-
-      {mutation.isLoading && (
+      <button onClick={() => deleteMutation.mutate(post.id)}>
+        Delete
+      </button>{" "}
+      <button onClick={() => updateMutation.mutate(post.id)}>
+        Update title
+      </button>
+      {deleteMutation.isLoading && (
         <p style={{ color: "purple" }}>Deleting Post...</p>
       )}
-      {mutation.isSuccess && (
-        <p style={{ color: "green" }}>{mutation?.data?.message as string}</p>
+      {deleteMutation.isSuccess && (
+        <p style={{ color: "green" }}>
+          {deleteMutation?.data?.message as string}
+        </p>
       )}
-      {mutation.isError && (
+      {deleteMutation.isError && (
         <p style={{ color: "red" }}>
           Oops! Could not delete post id: {post.userId}
         </p>
       )}
-
+      {updateMutation.isLoading && (
+        <p style={{ color: "purple" }}>Updating Post...</p>
+      )}
+      {updateMutation.isSuccess && (
+        <p style={{ color: "green" }}>
+          {updateMutation?.data?.message as string}
+        </p>
+      )}
+      {updateMutation.isError && (
+        <p style={{ color: "red" }}>Oops! Could not update post</p>
+      )}
       <p>{post.body}</p>
       <h4>Comments</h4>
-
       {data.map((comment) => (
         <li key={comment.id}>
           {comment.email}: {comment.body}
