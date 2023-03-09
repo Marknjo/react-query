dcfe270d79da12b2c48cd7fa1e6ffa74e10db625
Marknjo <marknjo7@gmail.com>
2023-03-09 16:46:07 +0300

    feat(Inifinite): demo-1 infinite scroll using SWAPI people

--- infinite-swapi/src/people/InfinitePeople.tsx
+++ infinite-swapi/src/people/InfinitePeople.tsx
@@ -1,13 +1,66 @@
+import { useInfiniteQuery } from "@tanstack/react-query";
 import InfiniteScroll from "react-infinite-scroller";
 import { Person } from "./Person";
 
-const initialUrl = "https://swapi.dev/api/people/";
+let initUlr = "https://swapi.dev/api/people";
+
 const fetchUrl = async (url: string) => {
   const response = await fetch(url);
   return response.json();
 };
 
 export function InfinitePeople() {
-  // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  const {
+    data,
+    isFetching,
+    isLoading,
+    isError,
+    error,
+    fetchNextPage,
+    hasNextPage,
+  } = useInfiniteQuery({
+    queryKey: ["swapi-persons"],
+    queryFn: () => fetchUrl(initUlr),
+    getNextPageParam: (lastPage) => lastPage.next || undefined,
+  });
+
+  if (isLoading) {
+    return <p className='loading'>Loading...</p>;
+  }
+
+  if (isError) {
+    return (
+      <>
+        <h4>Error Fetching</h4>
+        <p>{error?.toString().replace("Error:", "")}</p>
+      </>
+    );
+  }
+
+  return (
+    <>
+      {isFetching && <p className='loading'>Loading more...</p>}
+      {!hasNextPage && <h4>Nothing more to load😔</h4>}
+      <InfiniteScroll
+        loadMore={fetchNextPage as (page: number) => void}
+        hasMore={hasNextPage}>
+        {data.pages.map((pageData, i) =>
+          pageData.results.map(
+            (person: {
+              name: string;
+              eye_color: string;
+              hair_color: string;
+            }) => (
+              <Person
+                key={person.name}
+                name={person.name}
+                eyeColor={person.eye_color}
+                hairColor={person.hair_color}
+              />
+            )
+          )
+        )}
+      </InfiniteScroll>
+    </>
+  );
 }
