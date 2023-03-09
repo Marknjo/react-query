b2ba368e7c87124474a41c2691d8ec3dde62e142
Marknjo <marknjo7@gmail.com>
2023-03-09 16:59:18 +0300

    feat(Inifinite): demo-2 infinite scroll using SWAPI species

--- infinite-swapi/src/species/InfiniteSpecies.tsx
+++ infinite-swapi/src/species/InfiniteSpecies.tsx
@@ -1,3 +1,5 @@
+import { useInfiniteQuery } from "@tanstack/react-query";
+import React, { useId } from "react";
 import InfiniteScroll from "react-infinite-scroller";
 import { Species } from "./Species";
 
@@ -8,6 +10,64 @@ const fetchUrl = async (url: string) => {
 };
 
 export function InfiniteSpecies() {
-  // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  const pgDataKey = useId();
+  const {
+    data,
+    isLoading,
+    isFetching,
+    hasNextPage,
+    fetchNextPage,
+    isError,
+    error,
+  } = useInfiniteQuery({
+    queryKey: ["SWAPI-Species"],
+    queryFn: () => fetchUrl(initialUrl),
+    getNextPageParam: (lastPage) => lastPage.next || undefined,
+  });
+
+  if (isLoading) {
+    return <p className='loading'>Loading...</p>;
+  }
+
+  if (isError) {
+    return (
+      <>
+        <h4>Error Loading SWAPI Species</h4>
+        <p>{error?.toString().replace("Error:", "")}</p>
+      </>
+    );
+  }
+
+  return (
+    <>
+      {isFetching && <p className='loading'>Loading more...</p>}
+      {!hasNextPage && <h4>Nothing more to load😔</h4>}
+      <InfiniteScroll
+        hasMore={hasNextPage}
+        loadMore={fetchNextPage as (page: number) => void}>
+        {data.pages.map((pageData, i) => (
+          <React.Fragment key={`${i + pgDataKey}`}>
+            {pageData.results.map(
+              (
+                species: {
+                  name: string;
+                  language: string;
+                  average_lifespan: number;
+                  url: string;
+                },
+                i: number
+              ) => (
+                <Species
+                  key={`${i}-${species.url}-${species.name}`}
+                  name={species.name}
+                  language={species.language}
+                  averageLifespan={species.average_lifespan}
+                />
+              )
+            )}
+          </React.Fragment>
+        ))}
+      </InfiniteScroll>
+    </>
+  );
 }
