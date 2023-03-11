544fa45a8ff59fd5a143e8029c972cb96243a590
Marknjo <marknjo7@gmail.com>
2023-03-11 17:56:03 +0300

    feat(spa): prefect appointments

--- lazy-days-spa/client/src/components/appointments/hooks/useAppointments.ts
+++ lazy-days-spa/client/src/components/appointments/hooks/useAppointments.ts
@@ -1,7 +1,7 @@
 // @ts-nocheck
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import dayjs from 'dayjs';
-import { Dispatch, SetStateAction, useState } from 'react';
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
 
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
@@ -63,18 +63,31 @@ export function useAppointments(): UseAppointments {
   /** ****************** END 2: filter appointments  ******************** */
   /** ****************** START 3: useQuery  ***************************** */
   // useQuery call for appointments for the current monthYear
+  const queryClient = useQueryClient();
+  useEffect(() => {
+    const nextMonthYear = getNewMonthYear(monthYear, 1);
+    queryClient.prefetchQuery({
+      queryKey: [
+        queryKeys.appointments,
+        nextMonthYear.year,
+        nextMonthYear.month,
+      ],
+      queryFn: () => getAppointments(monthYear.year, monthYear.month),
+    });
+  }, [queryClient, monthYear]);
 
-  // TODO: update with useQuery!
   // Notes:
   //    1. appointments is an AppointmentDateMap (object with days of month
   //       as properties, and arrays of appointments for that day as values)
   //
   //    2. The getAppointments query function needs monthYear.year and
   //       monthYear.month
-  const { data: appointments = [] } = useQuery({
+  const { data: appointments = {} } = useQuery({
     queryKey: [queryKeys.appointments, monthYear.year, monthYear.month],
     queryFn: () => getAppointments(monthYear.year, monthYear.month),
   });
+
+  console.log(appointments);
 
   /** ****************** END 3: useQuery  ******************************* */
 
