895ac93769275784689f21fde557154398891dc5
Marknjo <marknjo7@gmail.com>
2023-03-13 12:15:33 +0300

    feat(spa): filter staff by role

--- lazy-days-spa/client/src/components/staff/hooks/useStaff.ts
+++ lazy-days-spa/client/src/components/staff/hooks/useStaff.ts
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
-import { Dispatch, SetStateAction, useState } from 'react';
+import { Dispatch, SetStateAction, useCallback, useState } from 'react';
 
 import type { Staff } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
@@ -22,10 +22,17 @@ export function useStaff(): UseStaff {
   // for filtering staff by treatment
   const [filter, setFilter] = useState('all');
 
-  // TODO: get data from server via useQuery
+  // Filter staff
+  const selectFn = useCallback(
+    (data: Staff[]) => filterByTreatment(data, filter),
+    [filter],
+  );
+
+  // get data from server via useQuery
   const { data: staff = [] } = useQuery({
     queryKey: [queryKeys.staff],
     queryFn: getStaff,
+    select: filter === 'all' ? undefined : selectFn,
   });
 
   return { staff, filter, setFilter };
