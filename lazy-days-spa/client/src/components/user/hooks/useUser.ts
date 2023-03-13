d2c76e14ddb41053a197c299eed55f99b10cb2e5
Marknjo <marknjo7@gmail.com>
2023-03-13 13:12:49 +0300

    feat(spa): partial implementation of authentication

--- lazy-days-spa/client/src/components/user/hooks/useUser.ts
+++ lazy-days-spa/client/src/components/user/hooks/useUser.ts
@@ -1,4 +1,6 @@
+import { useQuery } from '@tanstack/react-query';
 import { AxiosResponse } from 'axios';
+import { useState } from 'react';
 
 import type { User } from '../../../../../shared/types';
 import { axiosInstance, getJWTHeader } from '../../../axiosInstance';
@@ -9,16 +11,16 @@ import {
   setStoredUser,
 } from '../../../user-storage';
 
-// async function getUser(user: User | null): Promise<User | null> {
-//   if (!user) return null;
-//   const { data }: AxiosResponse<{ user: User }> = await axiosInstance.get(
-//     `/user/${user.id}`,
-//     {
-//       headers: getJWTHeader(user),
-//     },
-//   );
-//   return data.user;
-// }
+async function getUser(user: User | null): Promise<User | null> {
+  if (!user) return null;
+  const { data }: AxiosResponse<{ user: User }> = await axiosInstance.get(
+    `/user/${user.id}`,
+    {
+      headers: getJWTHeader(user),
+    },
+  );
+  return data.user;
+}
 
 interface UseUser {
   user: User | null;
@@ -27,17 +29,28 @@ interface UseUser {
 }
 
 export function useUser(): UseUser {
-  // TODO: call useQuery to update user data from server
-  const user = null;
+  const [user, setUser] = useState<User | null>(getStoredUser());
+  // call useQuery to update user data from server
+  useQuery({
+    enabled: !!user,
+    queryKey: [queryKeys.user],
+    queryFn: () => getUser(user),
+    onSuccess: (data) => setUser(data),
+  });
 
   // meant to be called from useAuth
   function updateUser(newUser: User): void {
-    // TODO: update the user in the query cache
+    // update the user in the query cache
+    setUser(newUser);
+
+    // Update user in local storage
+    setStoredUser(newUser);
   }
 
   // meant to be called from useAuth
   function clearUser() {
     // TODO: reset user to null in query cache
+    clearStoredUser();
   }
 
   return { user, updateUser, clearUser };
